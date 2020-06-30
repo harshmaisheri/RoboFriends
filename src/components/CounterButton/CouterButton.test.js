b411115d59c4f9b3bacc1d09305c099d898b3281
Harsh Maisheri <harshmaisheri@gmail.com>
2020-06-30 14:32:16 +0530

    Impoved Project Structure

--- src/components/CounterButton/CouterButton.test.js
+++ src/components/CounterButton/CouterButton.test.js
@@ -5,11 +5,6 @@ import CounterButton from "./CounterButton";
 const mockColor = "red";
 const Wrapper = shallow(<CounterButton color={mockColor} />);
 
-it("Expect to Render Card List", (done) => {
-  expect(Wrapper.debug()).toMatchSnapshot();
-  done();
-});
-
 it("Correctly increments the counter number", (done) => {
   Wrapper.find('[id="counter"]').simulate("click");
   expect(Wrapper.state()).toEqual({ count: 1 });
